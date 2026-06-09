d94d2fd8acb506e20c8bd0329a70c8a2143e948a
--chitrika003 <--cgchitrika@gmail.com>
2026-06-09 11:50:15 +0530

    Sloved Leetcode 1929 ConcatinationofArray

---
+++ src/com/implementation/ConcatinationofArray.java
@@ -0,0 +1,28 @@
+package com.implementation;
+
+import java.util.Arrays;
+
+public class ConcatinationofArray {
+
+    public  static int[] concatination(int[] nums){
+        //int j=0;
+        int n=nums.length;
+        int[] ans = new int[2*nums.length];
+        for(int i=0;i<nums.length;i++) {
+//			if(j>nums.length-1) {
+//				j=0;
+//			}
+            ans[i]=nums[i];
+            ans[i+n]=nums[i];
+        }
+        return  ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums= {1,2,3};
+        // TODO Auto-generated method stub
+        System.out.println(Arrays.toString(concatination(nums)));
+
+    }
+
+}
