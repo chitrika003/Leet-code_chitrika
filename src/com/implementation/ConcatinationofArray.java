e6e35d3c61507dc26154efd86ac97b89e2f33f63
Chitrika M <177418364+chitrika003@users.noreply.github.com>
2026-06-09 11:36:09 +0530

    Update ConcatinationofArray.java

--- src/com/implementation/ConcatinationofArray.java
+++ src/com/implementation/ConcatinationofArray.java
@@ -1,1 +1,18 @@
 The LeetCode problem Concatenation of Array 1929
+package com.implementation;
+
+public class ConcatinationofArray {
+    static public int[] concatination(int[] nums){
+        int[] ans = new int[nums.length*2];
+        for(int i=0;i<nums.length;i++){
+            ans[i]=nums[i];
+            ans[i+nums.length]=nums[i];
+        }
+        return ans;
+    }
+
+    public  static void main() {
+        int[] num={1,2,3};
+        System.out.println(concatination(num));
+    }
+}
