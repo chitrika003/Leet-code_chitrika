6a627d9b1d5935c65dad0de52b39cc0f8681e041
--chitrika003 <--cgchitrika@gmail.com>
2026-06-09 11:22:46 +0530

    Sloved Leetcode 1920 problem on Build Array from permutation

---
+++ src/com/implementation/ConcatinationofArray.java
@@ -0,0 +1,17 @@
+package com.implementation;
+
+public class ConcatinationofArray {
+    static public int[] concatination(int[] nums){
+        int[] ans = new int[nums.length*2];
+        for(int i=0;i<nums.length;i++){
+            ans[i]=nums[i];
+            ans[i+nums.length]=nums[i];
+        }
+        return ans;
+    }
+
+    public  static void main() {
+        int[] num={1,2,3};
+        System.out.println(concatination(num));
+    }
+}
