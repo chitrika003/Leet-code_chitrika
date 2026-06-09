b3c83c02892a52cf6cbe16a896e13904e330222d
--chitrika003 <--cgchitrika@gmail.com>
2026-06-09 13:13:33 +0530

    sloved the leet code problem 1480 runing Sum of 1d Array and Optimized it

---
+++ src/com/arrays/LC1480_RunningSumOf1dArray.java
@@ -0,0 +1,33 @@
+package com.arrays;
+
+import java.util.Arrays;
+
+public class LC1480_RunningSumOf1dArray {
+    public static int[] runningsumof1dArrays(int[] nums){
+        int[] ans = new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            int sum=0;
+            for(int j=0;j<=i;j++){
+                sum=sum+nums[j];
+            }
+            ans[i]=sum;
+        }
+        return ans;
+    }
+    public static int[] runningsumof1dArraysOptimized(int[] nums){
+        int[] ans = new int[nums.length];
+        int sum=0;
+        int j=0;
+        for(int i=0;i<nums.length;i++){
+            sum=sum+nums[i];
+            ans[j]=sum;
+            j++;
+        }
+
+        return ans;
+    }
+    public static void main() {
+        int[] num={1,2,3,4};
+        System.out.println(Arrays.toString(runningsumof1dArraysOptimized(num)));
+    }
+}
