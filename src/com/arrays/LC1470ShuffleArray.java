99899de906689a88a492118339b168c6691b13e5
--chitrika003 <--cgchitrika@gmail.com>
2026-06-11 20:24:37 +0530

    sloved the leet code problem 1470 ShuffelArray

---
+++ src/com/arrays/LC1470ShuffleArray.java
@@ -0,0 +1,30 @@
+package com.arrays;
+
+import java.util.Arrays;
+
+public class LC1470ShuffleArray {
+    public static int[] shuffle(int[] nums, int n){
+        int[] ans=new int[nums.length];
+        int j=n;
+        int k=0;
+        for(int i=0;i<nums.length;i++){
+            if(i%2==0){
+                ans[i]=nums[k];
+                k++;
+            }
+            else{
+                ans[i]=nums[j];
+                j++;
+            }
+
+        }
+
+        return ans;
+
+    }
+
+    public  static void main() {
+int[] nums={2,5,1,3,4,7};
+        System.out.println(Arrays.toString(shuffle(nums,3)));
+    }
+}
